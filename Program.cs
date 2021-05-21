a85ff611e0b43bbefebe5ab00fd81a0ea0606b25
Laurel <lbentleym1992@gmail.com>
2021-05-21 14:22:59 -0400

    adding files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+Console.Write("First Name >");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name >");
+string lastName = Console.ReadLine();
+
+string reverse = "";
+string fullName = firstName + " " + lastName;
+
+for (int i = fullName.Length - 1; i >= 0; i--)
+{
+    reverse += fullName[i];
+}
+
+Console.WriteLine(reverse);
+
+// for (Array reverseName = lastName().reverse() + " " + firstName().reverse()) ;
+// {
+//     Console.WriteLine(reverseName);
+// }
+
+// string initials = firstName[0] + " " + lastName[0];
+// Console.WriteLine(initials);
+
+// for (int num = 10; num >= 1; num--)
+// {
+//     Console.WriteLine(num);
+// }
+
+/*
+System.Console.Write("Who would you like to say hello to? ");
+
+string name = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    System.Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    System.Console.WriteLine($"Hello, {name}!");
+}
+*/
+
